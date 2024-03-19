1cd36fb950463e16920eeca2837e45753fc0b2eb
lcht2701 <lecanhhoangthanh@gmail.com>
2024-03-19 11:17:23 +0700

    feat: add serilog package + config program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,42 @@
 using DataAccess.Context;
+using DataAccess.Repositories;
+using FluentValidation.AspNetCore;
+using Serilog;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
+var corsPolicy = "CorsPolicy";
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/ToDoApp-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
 services.AddControllers();
+services.AddFluentValidationAutoValidation();
+services.AddFluentValidationClientsideAdapters();
+services.AddAutoMapper(Assembly.GetExecutingAssembly());
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 services.AddDbContext<ApplicationDbContext>();
+
+#region Dependency Injection
+services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+#endregion
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicy,
+        policy =>
+        {
+            policy.WithOrigins("*")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -20,7 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
+//app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
